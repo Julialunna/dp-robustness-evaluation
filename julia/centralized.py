71914b18ea2751c11ed47aaab0ad4d03664f4513
Julialunna <juliaaluna1@gmail.com>
2026-05-07 10:07:09 -0300

    changing training to use MLP instead of CNN

---
+++ julia/centralized.py
@@ -0,0 +1,86 @@
+from datasets import load_dataset
+import torch
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, Normalize, ToTensor
+import train
+from opacus import PrivacyEngine
+import parameters_centralized
+
+def load_dataset_centralized(dataset, batch_size):
+    pytorch_transforms = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
+
+    
+    def apply_transforms(batch):
+        batch["image"] = [pytorch_transforms(img) for img in batch["image"]]
+        return batch
+
+    train_data = dataset["train"].with_transform(apply_transforms)
+    test_data = dataset["test"].with_transform(apply_transforms)
+
+    trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    testloader = DataLoader(test_data, batch_size=batch_size)
+    
+    return trainloader, testloader
+
+
+
+def run_centralized_dp(trainloader, testloader, num_classes, epochs:int, lr:float, momentum:float=0.9):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = train.MLP(  input_size=28 * 28,
+    hidden_size=256,
+    num_classes=num_classes)
+
+    optim = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+    
+    privacy_engine = None
+
+    if parameters_centralized.USE_DP:
+        privacy_engine = PrivacyEngine(secure_mode=False)
+
+        model, optim, trainloader = privacy_engine.make_private(
+            module=model,
+            optimizer=optim,
+            data_loader=trainloader,
+            noise_multiplier=parameters_centralized.NOISE_MULTIPLIER,
+            max_grad_norm=parameters_centralized.MAX_GRAD_NORM,
+        )
+
+    model.to(device)
+
+    # Scheduler de Learning Rate
+
+    for e in range(epochs):
+        print(f"Training epoch {e} ...")
+        # train.train(model, trainloader, optim, device)
+        if parameters_centralized.USE_DP:
+            epsilon = train.train(
+                model,
+                trainloader,
+                privacy_engine,
+                optim,
+                parameters_centralized.TARGET_DELTA,
+                device=device,
+            )
+            print(f"  ε = {epsilon:.2f}")
+        else: 
+             train.train(
+                model,
+                trainloader,
+                privacy_engine,
+                optim,
+                parameters_centralized.TARGET_DELTA,
+                device=device,
+            )
+                
+
+    loss, accuracy = train.test(model, testloader, device)
+    print(f"Loss: {loss}")
+    print(f"Accuracy: {accuracy*100}%")
+
+
+
+
+
+my_dataset = load_dataset("ylecun/mnist")
+trainloader, testloader = load_dataset_centralized(my_dataset, batch_size=parameters_centralized.BATCH_SIZE)
+run_centralized_dp(trainloader, testloader, 10, epochs=parameters_centralized.EPOCHS, lr=parameters_centralized.LR)
---
+++ julia/server.py
@@ -0,0 +1,64 @@
+"""opacus: Training with Sample-Level Differential Privacy using Opacus Privacy Engine."""
+
+import logging
+from typing import List, Tuple
+
+from flwr.common import Context, Metrics, ndarrays_to_parameters, NDArrays, Parameters
+from flwr.server import ServerApp, ServerAppComponents, ServerConfig
+from flwr.server.strategy import FedAvg, FedProx
+from flwr.common import parameters_to_ndarrays
+
+import numpy as np
+from pathlib import Path
+
+import train
+import parameters_federated
+
+
+# Opacus logger seems to change the flwr logger to DEBUG level. Set back to INFO
+logging.getLogger("flwr").setLevel(logging.INFO)
+
+
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+    aggregated_accuracy = sum(accuracies) / sum(examples)
+    print(f"\n---> [Servidor] Acurácia da Rodada: {aggregated_accuracy * 100:.2f}%\n")
+    return {"accuracy": aggregated_accuracy}
+
+
+def get_evaluate_fn():
+    def evaluate(server_round: int, parameters: NDArrays, config: dict):
+        if server_round == parameters_federated.NUM_SERVER_ROUNDS:
+            print(f"\n[Servidor] Salvando modelo da última rodada ({server_round})...")
+            
+            save_path = Path("./modelos/modelo_final_FL_DP.npy")
+            save_path.parent.mkdir(parents=True, exist_ok=True)
+            
+            params_array = np.empty(len(parameters), dtype=object)
+            for i, arr in enumerate(parameters):
+                params_array[i] = arr
+            
+            np.save(save_path, params_array, allow_pickle=True)
+            print(f"[Servidor] Pesos salvos com sucesso em '{save_path}'")
+        
+        return None
+    return evaluate
+
+def server_fn(context: Context) -> ServerAppComponents:
+    num_rounds = parameters_federated.NUM_SERVER_ROUNDS
+
+    ndarrays = train.get_weights(train.MLP(parameters_federated.NUM_CLASSES))
+    parameters = ndarrays_to_parameters(ndarrays)
+
+    strategy = FedAvg(
+        fraction_fit=0.1,
+        fraction_evaluate=0.1,
+        evaluate_metrics_aggregation_fn=weighted_average,
+        initial_parameters=parameters,
+    )
+    config = ServerConfig(num_rounds=num_rounds)
+    return ServerAppComponents(config=config, strategy=strategy)
+
+
+server_app = ServerApp(server_fn=server_fn)
